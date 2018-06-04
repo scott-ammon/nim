220c97421236415153050a59419564606224aab1
Scott Ammon <scott.o.ammon@gmail.com>
2018-06-04 13:45:39 -0700

    added js functionality to check for clicked item and remove it from the associated heap

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+var heapObj = {
+	"heap-one": 3,
+	"heap-two": 5,
+	"heap-three": 7
+};
+
+function removeItem() {
+  console.log($(this).parent().attr('id'));
+
+  // find heap of object and remove it from heap array
+  var heapOfItem = $(this).parent().attr('id');
+  heapObj[heapOfItem]--;
+  console.log(heapObj);
+ 
+  // remove the clicked item from the correct heap
+  $(this).remove();
+};
 
 
 $(document).ready(function() {
-  console.log('working!');
+  console.log('js is working');
 
-  $(".item").draggable();
+  $(".item").on('click', removeItem);
 
-  $(".item").on('click', function() {
-    $(this).remove();
-  });
 });
