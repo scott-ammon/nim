9027c0dfe0f0a7383e6808766de5ef3f4ecad73f
Scott Ammon <scott.o.ammon@gmail.com>
2018-06-04 14:02:42 -0700

    added reset function to replace all the items on the board

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
+// Variable definitions
+
 var heapObj = {
 	"heap-one": 3,
 	"heap-two": 5,
 	"heap-three": 7
 };
 
-function removeItem() {
-  console.log($(this).parent().attr('id'));
 
+var initGame = function() {
+	// display modal window that selects 2p or 1 v comp, and enters usernames
+	// set mode of game play for either 2p or person v computer
+	// fade in game board and display which user should play first
+};
+
+var resetGame = function() {
+	// pop up modal window again
+	 $('.item').show();
+};
+
+var checkForWin = function() {
+  // enter this function if there is one heap left
+};
+
+var removeItem = function() {
+  
   // find heap of object and remove it from heap array
   var heapOfItem = $(this).parent().attr('id');
   heapObj[heapOfItem]--;
   console.log(heapObj);
  
-  // remove the clicked item from the correct heap
-  $(this).remove();
+  // hide the clicked item from the correct heap
+  $(this).hide();
 };
 
-
+// keep this section minimal, call functions outside
 $(document).ready(function() {
   console.log('js is working');
 
-  $(".item").on('click', removeItem);
+  $(".item").on("click", removeItem);
+
+  $(".reset").on("click", resetGame);
 
 });
